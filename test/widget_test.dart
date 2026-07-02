db884571eb24bb04269b65fb1c1667ecfa9d87b8
IsaPortuguez <isabelportuguez.dev@gmail.com>
2026-07-02 09:29:51 -0600

    Fix widget test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:first_app/main.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: GradientContainer(color1, color2),
+        ),
+      ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
